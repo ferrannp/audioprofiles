0f48b429d1b082dcb8a511c086cb586e60f57445
ferrannp <fnp.developer@gmail.com>
2015-03-30 22:49:24 +0200

    Update react 13 and deps.

--- js/components/Tabs.js
+++ js/components/Tabs.js
@@ -4,17 +4,21 @@ var MUITabs = Material.Tabs;
 var Tab = Material.Tab;
 var Router = require('react-router');
 
+_tabRoutes = {undefined: 0, "home": 0, "guide": 1, "roadmap": 2, "contact": 3};
+
 var Tabs = React.createClass({
-  mixins: [Router.Navigation],
+
+  contextTypes: {
+    router: React.PropTypes.func
+  },
 
   _onActive: function(tab){
-    this.transitionTo(tab.props.route);
+    this.context.router.transitionTo(tab.props.route);
   },
 
   render() {
     return (
-      <MUITabs tabWidth={150} initialSelectedIndex={null}>
-        {/*prop in next version of tabs*/}
+      <MUITabs tabWidth={150} initialSelectedIndex={_tabRoutes[this.context.router.getCurrentRoutes()[1].name]}>
         <Tab label="Home" route="home" onActive={this._onActive} />
         <Tab label="Guide" route="guide" onActive={this._onActive} />
         <Tab label="Roadmap" />
