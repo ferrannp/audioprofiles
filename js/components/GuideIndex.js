5a20a3ec00d77a0ea1691d700d7b837d001261f5
ferrannp <fnp.developer@gmail.com>
2015-07-21 18:28:36 +0200

    Change onClick for onTouchTap.

--- js/components/GuideIndex.js
+++ js/components/GuideIndex.js
@@ -13,7 +13,8 @@ var GuideIndex = React.createClass({
     router: React.PropTypes.func
   },
 
-  transitionTo(payload){
+  transitionTo(e, payload){
+    e.preventDefault();
     this.context.router.transitionTo('guide', {section: payload});
   },
 
@@ -22,27 +23,27 @@ var GuideIndex = React.createClass({
       <div className={classNames({'small-only': this.props.onlyMobile})}>
         <List subheader="Profiles"
               subheaderStyle={{color: CustomTheme.getPalette().primary1Color}}>
-          <ListItem onClick={() => this.transitionTo('first_steps')} primaryText="First steps" />
-          <ListItem onClick={() => this.transitionTo('priority_calls')}
+          <ListItem onTouchTap={(e) => this.transitionTo(e, 'first_steps')} primaryText="First steps" />
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'priority_calls')}
                     primaryText="Priority Calls"/>
-          <ListItem onClick={() => this.transitionTo('priority_notifications')}
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'priority_notifications')}
                     primaryText="Priority Notifications"/>
-          <ListItem onClick={() => this.transitionTo('silent_mode')} primaryText="Silent mode"/>
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'silent_mode')} primaryText="Silent mode"/>
         </List>
         <ListDivider />
         <List subheader="Tips &amp; Tricks"
               subheaderStyle={{color: CustomTheme.getPalette().primary1Color}}>
           <ListItem primaryText="Whatsapp notifications" disabled={true}
                     style={{color: Colors.minBlack}}/>
-          <ListItem onClick={() => this.transitionTo('messenger_notifications')}
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'messenger_notifications')}
                     primaryText="Facebook Messenger notifications"/>
         </List>
         <ListDivider />
         <List subheader="Features"
               subheaderStyle={{color: CustomTheme.getPalette().primary1Color}}>
-          <ListItem onClick={() => this.transitionTo('ringtones_contacts')}
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'ringtones_contacts')}
                     primaryText="Ringtones &amp; Contacts"/>
-          <ListItem onClick={() => this.transitionTo('scheduler')} primaryText="Scheduler"/>
+          <ListItem onTouchTap={(e) => this.transitionTo(e,'scheduler')} primaryText="Scheduler"/>
           <ListItem primaryText="Notification Widget" disabled={true}
                     style={{color: Colors.minBlack}}/>
         </List>
