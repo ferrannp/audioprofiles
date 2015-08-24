b2cea98430a1f0cc8cb2d2cf10d90997095ab0b0
ferrannp <fnp.developer@gmail.com>
2015-08-25 01:11:43 +0200

    refactor: Added controlled tabs!

--- js/components/Tabs.js
+++ js/components/Tabs.js
@@ -5,28 +5,38 @@ var Tab = Material.Tab;
 var Router = require('react-router');
 var Colors = Material.Styles.Colors;
 
-_tabRoutes = {undefined: 0, "home": 0, "guide": 1, "contact": 2};
-
 var Tabs = React.createClass({
 
   contextTypes: {
     router: React.PropTypes.func
   },
 
-  onActive: function(tab){
+  onActive(tab) {
+    //Sub-route of the Guide and user re-clicks the tab (handleTabsChange won't be fired)
+    if(this.context.router.getCurrentParams().section !== undefined){
+      this.context.router.transitionTo(tab.props.route);
+    }
+  },
+
+  handleTabsChange (value, e, tab){
     this.context.router.transitionTo(tab.props.route);
   },
 
   render() {
-    var index = _tabRoutes[this.context.router.getCurrentRoutes()[1].name];
+    var route = this.context.router.getCurrentRoutes()[1].name;
+    if(!route){
+      route = 'home';
+    }
 
-    //TODO update selected index when material-ui supports it
     return (
       <MUITabs tabItemContainerStyle={{height: '56px'}}
-               inkBarStyle={{backgroundColor: Colors.yellow500}} initialSelectedIndex={index}>
-        <Tab  label="Home" route="home" onActive={this.onActive} style={{fontWeight: '400'}}/>
-        <Tab label="Guide" route="guide" onActive={this.onActive} style={{fontWeight: '400'}}/>
-        <Tab label="Contact" route="contact" onActive={this.onActive} style={{fontWeight: '400'}}/>
+               inkBarStyle={{backgroundColor: Colors.yellow500}}
+               valueLink={{value: route,
+               requestChange: this.handleTabsChange}}>
+        <Tab label="Home" route="home" value="home" style={{fontWeight: '400'}} />
+        <Tab label="Guide" route="guide" value="guide" style={{fontWeight: '400'}}
+             onActive={this.onActive}/>
+        <Tab label="Contact" route="contact" value="contact" style={{fontWeight: '400'}}/>
       </MUITabs>
     );
   }
