5ccad5a58dbc3fea135363b35201d9fe2bdb5a2e
ferrannp <ferrannp@gmail.com>
2022-10-22 15:45:16 +0200

    feat: move to Next.js and change Privacy Policy

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../public/styles/main.scss";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="//fonts.googleapis.com/css?family=Roboto:400,300,500"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
