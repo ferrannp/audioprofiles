f89b32064d5e335c35b8e2590624fc1d85293a5d
ferrannp <ferrannp@gmail.com>
2022-12-11 14:41:55 +0100

    feat: meta title and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,29 @@ export default function Document() {
           rel="stylesheet"
           type="text/css"
         />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#009688" />
+        <title>Audio Profiles</title>
+        <meta property="og:title" content="Audio Profiles" key="title" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
       </Head>
       <body>
         <Main />
